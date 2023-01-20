b9162acc7d0b9cbe77d7dcdaac1a85db9d57ff24
amir <amirgafarov09@gmail.com>
2023-01-20 19:02:10 +0500

    full

---
+++ script.js
@@ -0,0 +1,32 @@
+
+var name = prompt('Введите имя')
+alert(name)
+
+var old = +prompt('сколько вам лет')
+alert(old)
+
+var ram = +prompt('Решите пример 6 + 3')
+alert(ram)
+
+var clam = +prompt('Решите пример 6 - 3')
+alert(clam)
+
+var damn = +prompt('Решите пример 6 * 3')
+alert(damn)
+
+var full = +prompt('Решите пример 6 / 3')
+alert(full)
+
+var pam = +prompt('Решите пример 10 % 3')
+alert(pam)
+
+var str = ('Посмотрите в консоль что-бы узнать ответы')
+alert(str)
+
+console.log('Введите имя = ' + (name));
+console.log('сколько вам лет = ' + (old));
+console.log('6 + 3 = 9 вашь ответ: ' + (ram));
+console.log('6 - 3 = 3 вашь ответ:' + (clam));
+console.log('6 * 3 = 18 вашь ответ: ' + (damn));
+console.log('6 / 3 = 2 вашь ответ: ' + (full));
+console.log('10 % 3 = 1 вашь ответ: ' + (pam));
